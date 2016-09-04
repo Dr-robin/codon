ce9e5406be0a51872c49034956c6b19209a93fed
robin <robin@makichan.moe>
2016-09-04 11:35:25 +0900

    일단 결과값은 출력함

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 
 var p = 'TGGCATGCGACCCACGCCAGTGCTCACGAAGCGGACGCAAACGATGCGACTGCAATACTGAATGCTGTTGCGGATTAT';
 var data = {F: ['TTT', 'TTC'], L: ['TTA', 'TTG', 'CTT', 'CTC', 'CTA', 'CTG'], I: ['ATT', 'ATC', 'ATA'], M: ['ATG'], V: ['GTT', 'GTC', 'GTA', 'GTG'], S: ['TCT', 'TCC', 'TCA', 'TCG', 'AGT', 'AGC'], P: ['CCT', 'CCC', 'CCA', 'CCG'], T: ['ACT', 'ACC', 'ACA', 'ACG'], A: ['GCT', 'GCC', 'GCA', 'GCG'], Y: ['TAT', 'TAC'], H: ['CAT', 'CAC'], Q: ['CAA', 'CAG'], N: ['AAT', 'AAC'], K: ['AAA', 'AAG'], D: ['GAT', 'GAC'], E: ['GAA', 'GAG'], C: ['TGT', 'TGC'], W: ['TGG'], R: ['CGT', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'], G: ['GGT', 'GGC', 'GGA', 'GGG']};
+var result = '';
 for(var i = 0; i < p.length / 3; i++) {
 	var t = p.substr(i * 3, 3);
-	console.log(t);
+	for(var j in data) {
+		if(data[j].indexOf(t) != -1) {
+			result += j;
+		}
+	}
 }
+console.log(result);
